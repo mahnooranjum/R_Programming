1113572afd50fe9b51b04ce4b6f8e8e356028519
Mahnoor Anjum <manomaq@gmail.com>
2020-04-20 01:37:03 +0500

    first commit

---
+++ R20200119_Basics.R
@@ -0,0 +1,12 @@
+# Vector 
+rm(list=ls())
+x1 = c(1,2,3,4,5,6)
+print(x1)
+
+genders = c("M", "F")
+
+sequence = seq(from=0, to=100, by=1/3)
+ 
+intSeq = 50:100
+
+mat <- matrix(x1, nrow=2, byrow=TRUE)
