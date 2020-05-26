c3b6d9bc37e7ed2f1bdbe0e9b1bb8f0a0e6db5f6
Mahnoor Anjum <manomaq@gmail.com>
2020-05-26 13:04:10 +0500

    updated regression repo

---
+++ Subject_Regression/R5_decisionTree.R
@@ -0,0 +1,52 @@
+
+RMSE = function(a, b){
+  sqrt(mean((a - b)^2))
+}
+# Import the dataset
+dataset = read.csv('polyRegression.csv')
+print(colnames(dataset))
+
+dataset <- dataset[,-1]
+print(colnames(dataset))
+
+# Splitting the dataset 
+library(caTools)
+split = sample.split(dataset$X, SplitRatio = 0.8)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+
+# Regressor
+# install.packages('rpart')
+library(rpart)
+regressor = rpart(formula = y ~ .,
+                  data = training_set,
+                  control = rpart.control(minsplit = (0.1*dim(dataset)[1])))
+# Predicting
+y_pred = predict(regressor, newdata = test_set)
+
+
+# Visualising
+library(ggplot2)
+ggplot() +
+  geom_point(aes(x = training_set$X, y = training_set$y),
+             colour = 'red') +
+  geom_line(aes(x = training_set$X, y = predict(regressor, newdata = training_set)),
+            colour = 'blue') +
+  ggtitle('X vs y (Training set)') +
+  xlab('X') +
+  ylab('y')
+
+# Visualising the Test set results
+library(ggplot2)
+ggplot() +
+  geom_point(aes(x = test_set$X, y = test_set$y),
+             colour = 'red') +
+  geom_line(aes(x = training_set$X, y = predict(regressor, newdata = training_set)),
+            colour = 'blue') +
+  ggtitle('X vs y (Test set)') +
+  xlab('X') +
+  ylab('y')
+
+
+RMSE(test_set$y, y_pred)
