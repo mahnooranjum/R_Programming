eedf6087c34069f3c644180572d4fb07c36a86f6
Mahnoor Anjum <manomaq@gmail.com>
2020-12-03 11:25:40 +0500

    added file

---
+++ Subject_Statistics/basics12.R
@@ -0,0 +1,6 @@
+# Construction of a matrix with 5 rows that contain the numbers 1 up to 20 and assign it to m
+
+m = matrix(1:20, byrow = TRUE, nrow = 5)
+
+# print m to the console
+print(m)
---
+++ Subject_Statistics/basics13.R
@@ -0,0 +1,8 @@
+# a vector called student_status
+student_status <- c("student", "not student", "student", "not student")
+
+# turn student_status into a factor and save it in the variable categorical_student
+categorical_student <- factor(student_status)
+
+# print categorical_student to the console
+print(categorical_student)
