32249f5c9aa07cfb8070a4daa60419b3c5f8d1cf
Mahnoor Anjum <manomaq@gmail.com>
2020-12-06 16:47:49 +0500

    added file

---
+++ Subject_Statistics/ci2.R
@@ -0,0 +1,12 @@
+# Assign the sample mean to object "m"
+m <- mean(samp)
+
+# Assign the standard deviation to object "s"
+s <- 8/sqrt(300)
+
+# Calculate the upper confidence interval
+m + 1.96*s
+
+# Calculate the lower confidence interval
+m - 1.96*s
+
