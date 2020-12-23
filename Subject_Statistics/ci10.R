34ad32d9adc7e55b7e981bf99954b9624060864b
Mahnoor Anjum <manomaq@gmail.com>
2020-12-23 23:11:18 +0500

    added file

---
+++ Subject_Statistics/ci10.R
@@ -0,0 +1,8 @@
+# Assign the value of p(1-p) to object "p"
+p <- 0.5 * (1 - 0.5)
+# Assign the value of the Z-score to new object "z"
+z <- 1.96
+# Assign the value of the margin of error squared to the new object "ms"
+ms <- 0.05^2
+# Calculate the neccessary sample size
+p*z^2 / ms
