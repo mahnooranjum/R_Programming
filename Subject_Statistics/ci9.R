125e9c5830690a74cd37eab96a1d606c61fe7a83
Mahnoor Anjum <manomaq@gmail.com>
2020-12-23 23:10:52 +0500

    added file

---
+++ Subject_Statistics/ci9.R
@@ -0,0 +1,8 @@
+# Assign the standard deviation squared to new object "ss"
+ss <- 1.25^2
+# Assign the value of the Z-score squared to new object "zs"
+zs <- 1.96^2
+# Assign the value of the margin of error squared to the new object "ms"
+ms <- 0.2^2
+# Calculate the neccessary sample size
+(ss*zs)/ms
