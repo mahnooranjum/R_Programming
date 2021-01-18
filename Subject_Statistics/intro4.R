72a92d33285ec44728e1fabb30cb4066e134ee61
Mahnoor Anjum <manomaq@gmail.com>
2021-01-18 17:21:06 +0500

    added file

---
+++ Subject_Statistics/intro4.R
@@ -0,0 +1,8 @@
+# make a function called multiply_a_b
+multiply_a_b <- function(a, b){
+  return (a * b)
+}
+
+# call the function multiply_a_b and store the result into a variable result
+
+result <- multiply_a_b(a = 3, b = 7)
