ce215676dc80617b5386a98faeaa08cea6cac6e9
Mahnoor Anjum <manomaq@gmail.com>
2021-01-18 17:15:22 +0500

    added file

---
+++ Subject_Statistics/distro9.R
@@ -0,0 +1,7 @@
+# calculate the mean and store it in the variable mean_chance
+
+mean_chance <- 25 * 0.2
+
+# calculate the standard deviation and store it in the variable std_chance
+
+std_chance <- sqrt(25 * 0.2 * (1 - 0.2))
