60481637db8d768c8f8deb642555edced2c2f382
Mahnoor Anjum <manomaq@gmail.com>
2021-01-18 17:14:55 +0500

    added file

---
+++ Subject_Statistics/distro4.R
@@ -0,0 +1,6 @@
+# calculate the expected probability value and assign it to the variable expected_score
+
+expected_score <- sum(data$outcome * data$probs)
+
+# print the variable expected_score
+expected_score
