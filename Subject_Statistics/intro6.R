a32f160af00042a78c187931362a3325ac3ca0f2
Mahnoor Anjum <manomaq@gmail.com>
2021-01-18 17:21:26 +0500

    added file

---
+++ Subject_Statistics/intro6.R
@@ -0,0 +1,7 @@
+# load in the dataset
+
+cars = read.csv("http://s3.amazonaws.com/assets.datacamp.com/course/uva/mtcars_semicolon.csv", sep = ';')
+
+# print the first 6 rows of the dataset
+
+print(head(cars))
