becb7f993f93f65fd93a8134a658128a6efb2b3c
Mahnoor Anjum <manomaq@gmail.com>
2021-01-18 17:21:14 +0500

    added file

---
+++ Subject_Statistics/intro5.R
@@ -0,0 +1,7 @@
+# load in the data and store it in the variable cars
+
+cars <-  read.csv("http://s3.amazonaws.com/assets.datacamp.com/course/uva/mtcars.csv")
+
+# print the first 6 rows of the dataset using the head() function
+
+print(head(cars))
