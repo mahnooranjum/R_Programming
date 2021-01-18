a2d3d68336fe4da23feb59214550f2768a11ca38
Mahnoor Anjum <manomaq@gmail.com>
2021-01-18 17:15:31 +0500

    added file

---
+++ Subject_Statistics/distro6.R
@@ -0,0 +1,2 @@
+# probability of a woman having a hair length of less than 20 centimeters
+round(pnorm(20, mean = 25, sd = 5),2)
---
+++ Subject_Statistics/distro8.R
@@ -0,0 +1,3 @@
+# calculate the z value and store it in the variable z_value
+
+z_value = (38 - 25) / 5
