ecbc9f5976eb2917660ad03c5267d270055f9820
Mahnoor Anjum <manomaq@gmail.com>
2021-01-18 17:18:29 +0500

    added file

---
+++ Subject_Statistics/ht3.R
@@ -0,0 +1,9 @@
+# calculate the z score and assign it to a variable called z_value
+z_value <- round((25.95 - 25)/ round(3.5/sqrt(40),2),2)
+
+# calculate the corresponding p value and store it in the variable called p_value
+
+p_value <- round(pnorm(z_value, lower.tail = FALSE),2)
+
+# print p_value to the console
+p_value
