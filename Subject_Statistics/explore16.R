53892f952dccc8cd8539000fc669fe689d9f0aef
Mahnoor Anjum <manomaq@gmail.com>
2021-01-18 17:18:14 +0500

    added file

---
+++ Subject_Statistics/explore16.R
@@ -0,0 +1,8 @@
+# Find the IQR of horsepower
+IQR(mtcars$hp)
+# Find the standard deviation of horsepower
+sd(mtcars$hp)
+# Find the IQR of miles per gallon
+IQR(mtcars$mpg)
+# Find the standard deviation of miles per gallon
+sd(mtcars$mpg)
